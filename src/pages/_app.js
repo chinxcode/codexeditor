36b66c60392caa1393499a1d9d0fbeca5e1bc3c2
Sachin <sachinsharma11175@gmail.com>
2025-04-19 00:40:02 +0530

    Add @vercel/analytics dependency and integrate Analytics component in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import "../styles/chatbot.css";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     return (
         <SessionProvider session={session}>
             <Component {...pageProps} />
+            <Analytics />
         </SessionProvider>
     );
 }
