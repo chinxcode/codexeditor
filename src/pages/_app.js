21a713baff23ca89c98fcb09df6d83a98004fe05
Sachin <sachinsharma11175@gmail.com>
2026-02-04 01:12:00 +0530

    feat: integrate Google Analytics for enhanced tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,29 @@ import "../styles/globals.css";
 import "../styles/chatbot.css";
 import { SessionProvider } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-            <Analytics />
-        </SessionProvider>
+        <>
+            <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-G39KFP1LZ7" />
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-G39KFP1LZ7');
+                    `,
+                }}
+            />
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+                <Analytics />
+            </SessionProvider>
+        </>
     );
 }
 
